1affc5cb66f97036c969f761114bdc1209082c27
Hanna <han.hin@gmx.at>
2019-02-28 12:35:03 +0100

    added menu component

---
+++ src/app/components/menue/menue.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menue',
+  templateUrl: './menue.component.html',
+  styleUrls: ['./menue.component.scss']
+})
+export class MenueComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
