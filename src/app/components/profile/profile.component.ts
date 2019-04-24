ee400319e7bda45e0cf2bc7d4487a8082c54fd52
Alexander Burghuber <alexander.burghuber@gmail.com>
2019-04-24 10:19:50 +0200

    merge fix

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,37 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MenuController} from '@ionic/angular';
 import {Person} from '../../entities/person';
 import {Router} from '@angular/router';
 import {User} from '../../entities/user';
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+    selector: 'app-profile',
+    templateUrl: './profile.component.html',
+    styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
 
-person: Person;
-optionalName: String;
+    person: Person;
+    optionalName: String;
 
-  constructor(private router : Router) {
-    const tempPerson = JSON.parse(localStorage.getItem('person'));
-    if (tempPerson) {
-      const person = tempPerson.person;
-      if (person) {
-        this.person = person;
-      }
+    constructor(private router: Router, private menu: MenuController) {
+        const tempPerson = JSON.parse(localStorage.getItem('person'));
+        if (tempPerson) {
+            const person = tempPerson.person;
+            if (person) {
+                this.person = person;
+            }
+        }
     }
-  }
 
- information(){
-    this.router.navigate(['/profile']);
- }
+    information() {
+        this.router.navigate(['/profile']);
+    }
 
+    open() {
+        this.menu.enable(true, 'profile').then(() => {
+            this.menu.open().then(() => {
+                console.log('Profile should be open in controller');
+                this.menu.isOpen().then(() => {
+                    console.log('Profile is open in controller');
+                });
+            });
+        });
+    }
 
+    close() {
+        this.menu.close('profile').then(() => {
+            console.log('Profile is closed');
+        });
+    }
 
-
-ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
