44980dde54541735d73ca86c6175eb39edefcd3f
Daniel Pfeffer <daniel.pfeffer@breathless-pictures.at>
2020-01-30 10:08:23 +0100

    Add timeSober and dateSober as variables see #27

--- src/app/components/display-permille/display-permille.component.ts
+++ src/app/components/display-permille/display-permille.component.ts
@@ -1,7 +1,5 @@
 import {Component} from '@angular/core';
 import {PermilleCalculationService} from '../../services/permille-calculation.service';
-import {StorageService} from '../../services/storage.service';
-import {DataService} from '../../services/data.service';
 
 @Component({
     selector: 'app-header',
@@ -11,10 +9,20 @@ import {DataService} from '../../services/data.service';
 export class DisplayPermilleComponent {
 
     currentPerMille: number;
+    timeSober: Date;
+    dateSober: Date;
 
-    constructor(pcs: PermilleCalculationService, private ss: StorageService, private d: DataService) {
+    constructor(pcs: PermilleCalculationService) {
         pcs.perMilleObservable.subscribe(item => {
             this.currentPerMille = Math.floor(item * 100) / 100;
+            this.timeSober = this.timeConverter((item / 0.1) * 60);
         });
+    }
+
+    private timeConverter(minutes: number) {
+        this.dateSober = new Date(new Date().getTime() + minutes * 60000);
+        // corrector because start time of unix is 1970 1 o' clock
+        minutes -= 60;
+        return new Date(minutes * 60 * 1000);
     }
 }
